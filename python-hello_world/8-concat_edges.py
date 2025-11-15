7d5114e36b614623e84598db5d8acfe7a4f51d7a
theehmedov <the.ehmedov008@gmail.com>
2025-11-15 17:01:18 +0000

    shell

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-words = str.split()
-print(words[4] + "-" + words[5] + " " + words[6] + " " + words[0])
-
+print(" ".join(str.split()[4:6] + str.split()[0:1] + str.split()[6:7]))
