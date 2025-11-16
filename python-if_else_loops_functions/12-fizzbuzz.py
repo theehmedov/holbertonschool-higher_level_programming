15aaff54672343ec8f272cf43ac6697e9a042312
theehmedov <the.ehmedov008@gmail.com>
2025-11-16 15:35:32 +0000

    shell

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 # comment
 def fizzbuzz():
     for i in range(1, 101):
-        if 1 % 3 == 0 and 1 % 5 == 0:
+        if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
         elif i % 3 == 0:
             print("Fizz", end=" ")
