1fb6bc2f8c7886177d21b85df7383499b9323ba7
theehmedov <the.ehmedov008@gmail.com>
2025-11-21 14:40:54 +0000

    python

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+a = len(sys.argv) -1
+def arg():
+    for i in range(1, 6):
+        print(f"{i}: {sys.argv[i]}")
+if a == 1:
+    print(f"{a} argument:")
+    print(arg())
+else:
+    print(f"{a} arguments:")
+    print(arg())
