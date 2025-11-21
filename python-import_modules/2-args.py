76cc7fb2198a288b18c2f588ec52992fa4ff6947
theehmedov <the.ehmedov008@gmail.com>
2025-11-21 15:25:59 +0000

    python

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,13 +2,15 @@
 import sys
 
 if __name__ == "__main__":
-    a = len(sys.argv) - 1
+    argv = sys.argv[1:]
+    argc = len(argv)
+
     if a == 1:
-        print(f"{a} argument:")
+        print("1 argument:")
         for i, arg in enumerate(argv, start=1):
             print(f"{i}: {arg}")
     elif a == 0:
-        print(f"{a} arguments.")
+        print("0 arguments.")
     else:
         print(f"{a} arguments:")
         for i, arg in enumerate(argv, start=1):
