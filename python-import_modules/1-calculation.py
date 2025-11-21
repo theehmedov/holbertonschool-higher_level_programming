508e5e4297b534025f4e3ba4fb9f0f3c6c422d4f
theehmedov <the.ehmedov008@gmail.com>
2025-11-21 14:07:44 +0000

    python

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,21 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import add
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
-from calculator_1 import sub
-if __name__ == "__main__":
-    a = 10
-    b = 5
     print("{} + {} = {}".format(a, b, sub(a, b)))
-from calculator_1 import mul
-if __name__ == "__main__":
-    a = 10
-    b = 5
     print("{} + {} = {}".format(a, b, mul(a, b)))
-from calculator_1 import div
-if __name__ == "__main__":
-    a = 10
-    b = 5
     print("{} + {} = {}".format(a, b, div(a, b)))
