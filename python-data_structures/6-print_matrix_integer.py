753ad17a825e7f530820405f11284b49287542af
theehmedov <the.ehmedov008@gmail.com>
2025-11-22 16:51:44 +0000

    python

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+for row in matrix:
+    for idx, val in enumerate(row):
+        if idx != len(row) - 1:
+            print("{:d}".format(val), end=" ")
+        else:
+            print("{:d}".format(val), end="")
+    print()  # yeni sətr
