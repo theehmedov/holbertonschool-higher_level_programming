8c18e4de11dff203e85ffa7ee5f2b2baeb6108a8
theehmedov <the.ehmedov008@gmail.com>
2025-11-22 19:26:49 +0000

    python

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    for i in my_list:
+        if i != 2:
+            new_list.append(i)
+        else:
+            new_list.append(replace)
+    return new_list
