1d620e177695126bc2e4bedae00770b74cb84e8f
theehmedov <the.ehmedov008@gmail.com>
2025-11-22 13:59:20 +0000

    python

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    a = sys.argv[1:]
+    l = len(a)
+    c = 0
+    if l == 1:
+        print("0")
+    else:
+        for i, arg in enumerate(a, start=1):
+            c = int(c) + int(arg)
+        print(int(c))
+
