af22dbedeee20ded75db5ebc073c8fe7cd9bbfe3
theehmedov <the.ehmedov008@gmail.com>
2025-12-03 10:58:29 +0000

    python-network

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Displays your GitHub user ID using Basic Authentication"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(username, token))
+
+    if response.status_code == 200:
+        print(response.json().get('id'))
+    else:
+        print(None)
