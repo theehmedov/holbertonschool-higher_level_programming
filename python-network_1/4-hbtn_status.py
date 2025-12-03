a24d40d07a5b0e28cc90739bf3e984cbe27aabd0
theehmedov <the.ehmedov008@gmail.com>
2025-12-03 10:34:34 +0000

    python-network

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status using requests"""
+import requests
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    response = requests.get(url)
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
